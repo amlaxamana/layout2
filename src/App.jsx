8c0f13a9a0e28b695ba51e8a26b9652efc59ad57
amlaxamana <amlaxamana.student@ua.edu.ph>
2025-09-16 20:51:19 +0800

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,34 @@ import './App.css'
 function App() {
 
   return (
-    <div className="MainContainer">
-      <div className="Section1">
+    <div className="mainContainer">
+      <div className="section1">
         <div className="Box1"></div>
         <div className="Box2">
           <div className="InnerBox1"></div>
-          <div className="InnerBox2"></div>
+          <div className="InnerBox1"></div>
         </div>
         <div className="Box3"></div>
+
       </div>
-      <div className="Section2">
-        <div className="leftside"></div>
-        <div className="rightside">
-          <div className="Box5"></div>
-          <div className="Box6"></div>
+      <div className="section2">
+        <div className="leftbox"></div>
+        <div className="rightboxes">
+          <div className="rightbox"></div>
+          <div className="rightbox"></div>
         </div>
       </div>
-      <div className="Section3">
-        <div className="Box7"></div>
-        <div className="Box8"></div>
-        <div className="Box9"></div>
-        <div className="Box10"></div>
+
+      <div className="section3">
+        <div className="boxA"></div>
+        <div className="boxB"></div>
+        <div className="boxC"></div>
+        <div className="boxD"></div>
       </div>
-    </div>
-  )
+
+
+      </div>
+    )
 }
 
-export default App
+      export default App
