73beb17102c75041d4c7c80e6755afea0d08056e
amlaxamana <amlaxamana.student@ua.edu.ph>
2025-09-18 20:33:15 +0800

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,71 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// UPPER SECTION
+function SideBox() {
+  return <div className="Box1"></div>
+}
+function Section1Box2InnerBox1(props) {
+  return <div className="InnerBox1">{props.name}</div>
+}
+
+function UpperBox2() {
+  return <div className="Box2">
+    <Section1Box2InnerBox1 name="Aaron" />
+    <Section1Box2InnerBox1 name="Laxamana" />
+  </div>
+}
+
+// LOWER SECTION
+function LowSideBox() {
+  return <div className="boxA"></div>
+}
+
+function Section3CenterBox(props) {
+  return <div className="boxB">{props.text}</div>
+}
+
+// DISPLAY 
+function TopSection() {
+  return <div className="section1">
+    <SideBox />
+    <UpperBox2 />
+    <SideBox />
+  </div>
+
+}
+
+function LowerSection() {
+  return <div className="section3">
+    <LowSideBox />
+    <Section3CenterBox text="C-PCIT9" />
+    <Section3CenterBox text="IT3A" />
+    <LowSideBox />
+  </div>
+}
+
+// MAIN FUNCTION
 function App() {
 
   return (
+    
     <div className="mainContainer">
-      <div className="section1">
-        <div className="Box1"></div>
-        <div className="Box2">
-          <div className="InnerBox1"></div>
-          <div className="InnerBox1"></div>
-        </div>
-        <div className="Box3"></div>
 
-      </div>
+      <TopSection />
+
       <div className="section2">
         <div className="leftbox"></div>
         <div className="rightboxes">
           <div className="rightbox"></div>
           <div className="rightbox"></div>
         </div>
-      </div>
-
-      <div className="section3">
-        <div className="boxA"></div>
-        <div className="boxB"></div>
-        <div className="boxC"></div>
-        <div className="boxD"></div>
-      </div>
-
 
       </div>
-    )
+      
+      <LowerSection />
+
+    </div>
+  )
 }
 
-      export default App
+export default App
